0dd70ef480bb8decfaabc6486e07bd1bd7da40ea
SergeyDobrev <dobrev.sergey@gmail.com>
2017-12-25 19:35:18 +0200

    3th commit

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function myfun() {
-
-$( '.moveloinerl2' ).slideDown(2000);
-	
+function myfun1() {
+	$('#index4').slideUp(3000);
 }
 
 function myfun2() {
+	$('#index3').slideUp(2000);
+}
 
-$( '.moveloinerl2' ).slideUp(2000);
-	
+
+function myfun3() {
+	$('#index2').slideUp(3000);
+	$('#index21').fadeOut(4000);
+		
+}
+
+function myfun4() {
+	$('#index1').slideUp(2000);
+}
+
+function myfun5() {
+	$('#index1').slideDown(2000);
+}
+
+function myfun6() {
+	$('#index2').slideDown(3000);
+	$('#index21').fadeIn(4000);	
+	$('#index3').slideDown(2000);	
+}
+
+function myfun7() {
+	$('#index4').slideDown(3000);
+}
+
+function myfun8() {
+	$('#index4').slideDown(3000);
 }
